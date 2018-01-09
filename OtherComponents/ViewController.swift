e2f935b82c2ae5715cf4eee3fad918788fa58711
Gaurav <gaurav.purohit024@gmail.com>
2018-01-09 15:35:55 +0530

    updated

--- OtherComponents/ViewController.swift
+++ OtherComponents/ViewController.swift
@@ -10,11 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchbtn: UISwitch!
+    
+    @IBOutlet weak var sliderObj: UISlider!
+    
+    @IBOutlet weak var progressObj: UIProgressView!
+    var timer:Timer!
+    var progressVL:Float!
+    
+    @IBOutlet weak var lblSlider: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sliderObj.minimumValue = 0
+        sliderObj.maximumValue = 1000
+        
+        progressVL = 0
+        progressObj.setProgress(0, animated: true)
+        
     }
-
+    
+    
+    @IBAction func slidervaluechange(_ sender: UISlider) {
+        
+        lblSlider.text = "vl = \(sender.value)"
+    }
+    
+    @IBAction func togglechange(_ sender: UISwitch) {
+        
+        if sender.isOn
+        {
+            print("on")
+         //   sender.setOn(false, animated: true)
+        } else {
+            print("off")
+          //  sender.setOn(true, animated: true)
+        }
+    }
+    @IBAction func btnstart(_ sender: UIButton) {
+        
+         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updatevl), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updatevl()  {
+        progressVL = progressVL + 0.1
+         progressObj.setProgress(progressVL, animated: true)
+        print(progressVL)
+        
+        if Int(progressVL) == 1 {
+            timer.invalidate()
+            
+            progressObj.isHidden = true
+            progressObj.removeFromSuperview()
+           
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
