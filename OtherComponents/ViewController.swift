e9020a22e31bee8fd54d2dd12024ac564e2f384b
Gaurav <gaurav.purohit024@gmail.com>
2018-01-09 14:58:29 +0530

    Initial Commit

---
+++ OtherComponents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OtherComponents
+//
+//  Created by Gaurav on 09/01/18.
+//  Copyright © 2018 Gaurav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
